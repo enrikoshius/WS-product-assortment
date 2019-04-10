3ad968ddcf53634e94f38189ee7cd13cb1ea6e2f
enrika <enrika.cergelyte@nortal.com>
2019-04-10 13:02:11 +0300

    test5

--- productassortment/src/main/java/mif/productassortment/repository/ProductRepository.java
+++ productassortment/src/main/java/mif/productassortment/repository/ProductRepository.java
@@ -6,15 +6,16 @@ import mif.productassortment.domain.Product;
 import mif.productassortment.domain.enumerator.ProductStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-	@Query("SELECT u.id, u.date_created, u.date_updated,  u.name, u.description, u.status FROM product u WHERE u.status LIKE CONCAT('%',:status,'%')")
-	List<Product> findByStatus(ProductStatus status);
-	@Query("SELECT u.id, u.date_created, u.date_updated,  u.name, u.description, u.status FROM product u WHERE u.name LIKE CONCAT('%',:name,'%')")
-	List<Product> findByName(String name);
-	@Query("SELECT u.id, u.date_created, u.date_updated,  u.name, u.description, u.status FROM product u WHERE u.description LIKE CONCAT('%',:description,'%')")
-	List<Product> findByDescription(String description);
+	@Query("SELECT u.status FROM product u WHERE u.status LIKE CONCAT('%',:status,'%')")
+	List<Product> findByStatus(@Param("status")ProductStatus status);
+	@Query("SELECT  u.name FROM product u WHERE u.name LIKE CONCAT('%',:name,'%')")
+	List<Product> findByName(@Param("name")String name);
+	@Query("SELECT u.description FROM product u WHERE u.description LIKE CONCAT('%',:description,'%')")
+	List<Product> findByDescription(@Param("description")String description);
 }
