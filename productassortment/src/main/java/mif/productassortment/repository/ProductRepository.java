d7effb2f761534d31cb97e5d34f1e0877f085e8a
enrika <enrika.cergelyte@nortal.com>
2019-04-10 12:40:58 +0300

    test2

--- productassortment/src/main/java/mif/productassortment/repository/ProductRepository.java
+++ productassortment/src/main/java/mif/productassortment/repository/ProductRepository.java
@@ -5,11 +5,16 @@ import java.util.List;
 import mif.productassortment.domain.Product;
 import mif.productassortment.domain.enumerator.ProductStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	@Query("SELECT u.status FROM Product u WHERE u.status LIKE CONCAT('%',:status,'%')")
 	List<Product> findByStatus(ProductStatus status);
+	@Query("SELECT u.name FROM Product u WHERE u.name LIKE CONCAT('%',:name,'%')")
 	List<Product> findByName(String name);
+	@Query("SELECT u.description FROM Product u WHERE u.description LIKE CONCAT('%',:description,'%')")
 	List<Product> findByDescription(String description);
 }
