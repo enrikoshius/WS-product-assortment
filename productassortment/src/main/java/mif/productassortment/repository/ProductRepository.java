9e7b7ddf0eeaf0281b01f4843675054197c604ab
enrikoshius <42121136+enrikoshius@users.noreply.github.com>
2019-05-07 21:44:06 +0300

    Update ProductRepository.java

--- productassortment/src/main/java/mif/productassortment/repository/ProductRepository.java
+++ productassortment/src/main/java/mif/productassortment/repository/ProductRepository.java
@@ -12,10 +12,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-	@Query("SELECT u.status FROM product u WHERE u.status LIKE CONCAT('%',:status,'%')")
+	@Query("SELECT u.status FROM Product u WHERE u.status LIKE CONCAT('%',:status,'%')")
 	List<Product> findByStatus(@Param("status")ProductStatus status);
-	@Query("SELECT  u.name FROM product u WHERE u.name LIKE CONCAT('%',:name,'%')")
+	@Query("SELECT  u.name FROM Product u WHERE u.name LIKE CONCAT('%',:name,'%')")
 	List<Product> findByName(@Param("name")String name);
-	@Query("SELECT u.description FROM product u WHERE u.description LIKE CONCAT('%',:description,'%')")
+	@Query("SELECT u.description FROM Product u WHERE u.description LIKE CONCAT('%',:description,'%')")
 	List<Product> findByDescription(@Param("description")String description);
 }
